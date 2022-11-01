7144d10289b33ebf0d05163ffd054e4788eb8c29
윤승재 <106107068+0poison@users.noreply.github.com>
2022-11-01 10:05:23 +0900

    Update TopMenuInterceptor.java
    로그인 하면 "로그아웃"이라는 메시지만 표시되고 로그인, 로그아웃 중에 하나만 나올 수 있도록 표시

--- src/main/java/kr/co/ezenac/interceptor/TopMenuInterceptor.java
+++ src/main/java/kr/co/ezenac/interceptor/TopMenuInterceptor.java
@@ -12,23 +12,27 @@ import kr.co.ezenac.beans.BoardInfoBean;
 import kr.co.ezenac.service.TopMenuService;
 
 public class TopMenuInterceptor implements HandlerInterceptor {
-	
+
 	// 인터셉터는 Bean 자동 주입이 안된다.
-	
+
 	private TopMenuService topMenuService;
-	
-	//자동 주입이 안되어 객체 생성 후 넣어준다.
+
+	private AdminBean loginAdminBean;
+
+	// 자동 주입이 안되어 객체 생성 후 넣어준다.
 	public TopMenuInterceptor(TopMenuService topMenuService, AdminBean loginAdminBean) {
 		this.topMenuService = topMenuService;
+		this.loginAdminBean = loginAdminBean;
 	}
-	
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		
+
 		List<BoardInfoBean> topMenuList = topMenuService.getTopMenuList();
 		request.setAttribute("topMenuList", topMenuList);
-		
+		request.setAttribute("loginAdminBean", loginAdminBean);
+
 		return true;
 	}
 
